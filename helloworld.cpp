360fa8c02b7e40a11a60ce859cf6f176d200d218
Lukas <lukasheim9@icloud.com>
2026-03-07 22:07:02 +0100

    init2

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,21 @@
+#include <iostream>
 #include <boost/asio.hpp>
 
 
+void print(const boost::system::error_code& /*e*/)
+{
+  std::cout << "Hello, world!" << std::endl;
+}
+
 int main()
 {
-    int x = 23;
+  boost::asio::io_context io;
 
+  boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
+  t.async_wait(&print);
+  std::cout << "lol\n";
 
+  io.run();
 
+  return 0;
 }
